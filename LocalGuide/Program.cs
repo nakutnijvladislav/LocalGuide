d2ce5c3dd73d911c1852a476297fbe36510113c1
nakutnijvladislav <nakutnijvladislav@gmail.com>
2026-03-22 13:34:17 +0200

    test ci

--- LocalGuide/Program.cs
+++ LocalGuide/Program.cs
@@ -38,6 +38,7 @@ namespace LocalGuide
                 }
             };
 
+
             // 2. Створення екземплярів сервісів
             var locationService = new LocationService(initialData);
             var authService = new UserAuth("my-very-secure-secret-key-123");
